e609303552adf862d319cb68d64a6ee9229012c3
zengwang <zengwang430521@gmail.com>
2021-09-23 16:56:08 +0800

    debug

--- debug.sh
+++ debug.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env bash
 
-
-    -x SH-IDC1-10-198-4-[100-103,116-119] \
-srun -p pat_earth --ntasks=8 \
+srun -p mm_human --ntasks=8 \
     --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
-    --job-name=res50 \
+    --job-name=pvtv2   python -u tools/train.py configs/pvtv2_coco_wholebody_256x192.py \
+    --work-dir=work_dirs/pvtv2 --resume-from=work_dirs/pvtv2/latest.pth --launcher="slurm"
+
+srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
+srun -p mm_human
+    --ntasks=8 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
+    --job-name=res50_0 python -u tools/train.py \
+    configs/res50_0.py --work-dir=work_dirs/res50_0  --resume-from=work_dirs/res50_0/latest.pth --launcher="slurm"
+
+    --job-name=my20_0 python -u tools/train.py configs/my20_2_0.py
+    --work-dir=work_dirs/my20_2_0 --resume-from=work_dirs/my20_2_0/latest.pth --launcher="slurm"
+
+    --job-name=pvtv2_0   python -u tools/train.py configs/pvtv2_0.py \
+    --work-dir=work_dirs/pvtv2_0 --resume-from=work_dirs/pvtv2_0/latest.pth --launcher="slurm"
+
+
+    --job-name=res50 python -u tools/train.py \
+    configs/wholebody/2d_kpt_sview_rgb_img/topdown_heatmap/coco-wholebody/res50_coco_wholebody_256x192.py \
+    --work-dir=work_dirs/res50  --resume-from=work_dirs/res50/latest.pth --launcher="slurm"
+
+    --job-name=my20 \
     python -u tools/train.py configs/my20_2_coco_wholebody_256x192.py --work-dir=work_dirs/my20_2 --launcher="slurm"
 
-    python -u tools/train.py configs/pvtv2_coco_wholebody_256x192.py --work-dir=work_dirs/pvtv2 --launcher="slurm"
+    --job-name=pvtv2   python -u tools/train.py configs/pvtv2_coco_wholebody_256x192.py \
+    --work-dir=work_dirs/pvtv2 --resume-from=work_dirs/pvtv2/latest --launcher="slurm"
 
-    python -u tools/train.py configs/wholebody/2d_kpt_sview_rgb_img/topdown_heatmap/coco-wholebody/res50_coco_wholebody_256x192.py \
-    --work-dir=work_dirs/res50 --launcher="slurm"
+
 
 
