85d28c2ca5d68af622612971c93a136f3833c521
zengwang <zengwang430521@gmail.com>
2021-10-27 23:01:33 +0800

    debug

--- debug.sh
+++ debug.sh
@@ -5,8 +5,8 @@
 srun -p mm_human \
     --ntasks=1 --gres=gpu:1 --ntasks-per-node=1 --cpus-per-task=5 --kill-on-bad-exit=1 \
 
-srun -p mm_human \
 srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
+srun -p mm_human \
     --ntasks=8 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
     --job-name=den0_att python -u tools/train.py configs/pvt3h2_den0_att_adamw.py --work-dir=work_dirs/den0_att --launcher="slurm"
 
