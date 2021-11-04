8f4157284e3057cd40fbf8f017e389566e95eba7
zengwang <zengwang430521@gmail.com>
2021-11-04 14:42:36 +0800

    debug

--- mmpose/models/backbones/pvt_v2_3h2_density_fix_sparse.py
+++ mmpose/models/backbones/pvt_v2_3h2_density_fix_sparse.py
@@ -17,7 +17,7 @@ from .utils_mine import (
     # farthest_point_sample
 )
 
-from .utils_mine import token_cluster_density_fixbug as token_cluster_density
+from .utils_mine import token_cluster_density_fixbug_sr as token_cluster_density
 from .utils_mine import token2map_agg_sparse as token2map_agg_mat
 from .utils_mine import map2token_agg_sparse_nearest as map2token_agg_fast_nearest
 from ..builder import BACKBONES
