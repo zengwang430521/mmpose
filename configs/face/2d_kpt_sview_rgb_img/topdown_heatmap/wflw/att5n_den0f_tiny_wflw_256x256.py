ea4c1cb79c66d5564610051c1759692953a980e6
zengwang <zengwang430521@gmail.com>
2021-11-06 16:36:36 +0800

    debug

--- configs/face/2d_kpt_sview_rgb_img/topdown_heatmap/wflw/att5n_den0f_tiny_wflw_256x256.py
+++ configs/face/2d_kpt_sview_rgb_img/topdown_heatmap/wflw/att5n_den0f_tiny_wflw_256x256.py
@@ -111,7 +111,8 @@ test_pipeline = val_pipeline
 
 data_root = 'data/wflw'
 data = dict(
-    samples_per_gpu=64,
+    # samples_per_gpu=64,
+    samples_per_gpu=32,
     workers_per_gpu=2,
     val_dataloader=dict(samples_per_gpu=32),
     test_dataloader=dict(samples_per_gpu=32),
