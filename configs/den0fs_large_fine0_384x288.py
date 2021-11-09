d5680ff9a8f213b9710ca56ff2b03b8f872a75f9
zengwang <zengwang430521@gmail.com>
2021-11-09 23:35:19 +0800

    debug

--- configs/den0fs_large_fine0_384x288.py
+++ configs/den0fs_large_fine0_384x288.py
@@ -134,7 +134,7 @@ test_pipeline = val_pipeline
 
 data_root = 'data/coco'
 data = dict(
-    samples_per_gpu=16,
+    samples_per_gpu=8,
     workers_per_gpu=2,
     val_dataloader=dict(samples_per_gpu=32),
     test_dataloader=dict(samples_per_gpu=32),
