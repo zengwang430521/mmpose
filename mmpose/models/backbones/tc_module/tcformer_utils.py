cb4b75e9fbf539a4d13e8435e3d2eb53959858f3
zengwang <zengwang430521@gmail.com>
2021-12-29 21:47:44 +0800

    debug

--- mmpose/models/backbones/tc_module/tcformer_utils.py
+++ mmpose/models/backbones/tc_module/tcformer_utils.py
@@ -2158,3 +2158,48 @@ def token_cluster_ats(input_dict, Ns, score, weight=None):
     weight_t = index_points(norm_weight, idx_agg)
 
     return x_out, idx_agg, weight_t
+
+
+# gaussian filtering
+def gaussian_filt(x, kernel_size=5, sigma=None):
+    if kernel_size < 3:
+        return x
+
+    if sigma is None:
+        sigma = 0.6 * kernel_size
+
+    channels = x.shape[1]
+
+    # Create a x, y coordinate grid of shape (kernel_size, kernel_size, 2)
+    x_coord = torch.arange(kernel_size, device=x.device)
+    x_grid = x_coord.repeat(kernel_size).view(kernel_size, kernel_size).contiguous()
+    y_grid = x_grid.t()
+    xy_grid = torch.stack([x_grid, y_grid], dim=-1).float()
+
+    mean = (kernel_size - 1) / 2.
+    variance = sigma ** 2.
+
+    # Calculate the 2-dimensional gaussian kernel which is
+    # the product of two gaussian distributions for two different
+    # variables (in this case called x and y)
+    gaussian_kernel = (1. / (2. * math.pi * variance)) * torch.exp(-torch.sum((xy_grid - mean) ** 2., dim=-1) / (2 * variance))
+
+    # Make sure sum of values in gaussian kernel equals 1.
+    gaussian_kernel = gaussian_kernel / torch.sum(gaussian_kernel)
+
+    # Reshape to 2d depthwise convolutional weight
+    gaussian_kernel = gaussian_kernel.view(1, 1, kernel_size, kernel_size).contiguous()
+    gaussian_kernel = gaussian_kernel.repeat(channels, 1, 1, 1)
+
+    pad = int((kernel_size - 1) // 2)
+
+    x = F.pad(x, (pad, pad, pad, pad), mode='replicate')
+    y = F.conv2d(
+        input=x,
+        weight=gaussian_kernel,
+        stride=1,
+        padding=0,
+        dilation=1,
+        groups=channels
+    )
+    return y
