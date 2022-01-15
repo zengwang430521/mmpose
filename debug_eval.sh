8b46006a970c9fbca9af34be9339af47e54aaee3
zengwang <zengwang430521@gmail.com>
2022-01-15 19:59:08 +0800

    debug

--- debug_eval.sh
+++ debug_eval.sh
@@ -3,6 +3,9 @@ srun -p pat_earth \
 srun -p mm_human --quotatype=auto\
     --ntasks=8 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
     --job-name=eval python -u tools/test.py  --launcher="slurm" \
+    configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrtc_bi_part_re7_w32_coco_256x192_fine.py \
+    work_dirs/coco/hrtc_bi_part_re7_32_fine/epoch_240.pth
+
     configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrtcformer_bi_w32_coco_256x192_scratch2.py \
     work_dirs/coco/hrtc_bi_w32_scratch2/epoch_50.pth
 
