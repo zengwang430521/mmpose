6898fdb9671bd4bd3fa3fb1cf37299ea7b49cb80
zengwang <zengwang430521@gmail.com>
2022-01-20 20:30:37 +0800

    debug

--- debug.sh
+++ debug.sh
@@ -14,14 +14,16 @@ srun -p mm_human \
 srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
     --ntasks=8 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
     --job-name=coco python -u tools/train.py  --launcher="slurm" \
+    configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrtc_bi_part_re11_w32_coco_256x192_scratch.py \
+    --work-dir=work_dirs/coco/hrtc_bi_part_re11_32
+
     configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrtc_bi_part_base.py \
     --work-dir=work_dirs/coco/hrtc_bi_part_base
 
     configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrtc_bi_part_re5_w32_coco_256x192_scratch.py \
     --work-dir=work_dirs/coco/hrtc_bi_part_re5_32
 
-    configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrtc_bi_part_re11_w32_coco_256x192_scratch.py \
-    --work-dir=work_dirs/coco/hrtc_bi_part_re11_32
+
 
     configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrtc_near_part_re9_w32_coco_256x192_scratch.py \
     --work-dir=work_dirs/coco/hrtc_near_part_re9_32
