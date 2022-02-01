208b69bdb73ffc0d0c0f26f85be8441df33f7e3e
zengwang <zengwang430521@gmail.com>
2022-02-01 21:08:32 +0800

    'debug'

--- debug.sh
+++ debug.sh
@@ -6,8 +6,7 @@ srun -p mm_human \
     --job-name=ae_att_coco python -u tools/train.py  --work-dir=work_dirs/ae_att_coco --launcher="slurm" \
     configs/body/2d_kpt_sview_rgb_img/associative_embedding/coco/den0_small_coco_512x512.py
 
-srun -p mm_human --quotatype=auto\
-srun -p pat_earth \
+srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
     --ntasks=16 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
     --job-name=train python -u tools/train.py  --launcher="slurm" \
     configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrtc_bi_part_re19_w32_coco_256x192_scratch.py \
